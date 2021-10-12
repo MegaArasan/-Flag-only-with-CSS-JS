ad5e378c871bca71a7017b27943db7e733a23e1b
Mega Arasan V <91074730+MegaArasan@users.noreply.github.com>
2021-10-12 03:22:50 -0700

    update

--- demo.js
+++ demo.js
@@ -71,46 +71,24 @@ const countries = [
 
 //  document.body.querySelector(".flag_details").append(country,population,region,capital)
 
- const div=document.createElement("div")
-    div.className="row"
-    const col=document.createElement("div")
-   div.className="col-12 col-sm-6 col-md-3 col-lg-3"
-    div.innerHTML=`
-    <div class="flag"></div>`
-    div.appendChild(col)
-document.body.append(div)
-    countries.forEach(element => {
-   const col=document.createElement("div")
-   div.className="col-12 col-sm-6 col-md-3 col-lg-3"
-    div.innerHTML=`
-    <div class="flag"></div>`
-    document.querySelector(".row").append(col)
-   
-   const image=document.createElement("img")
-   image.src=countries[i].image
-   image.alt="image"
-   document.querySelector(".flag").append(image)
-   
-   const flagdetails=document.createElement("div")
-   flagdetails.className="flag_details"
-   document.querySelector(".flag").append(flagdetails)
-   
-    const country=document.createElement("p")
-    country.className="heading"
-    country.innerText=countries[i].country;
-    document.body.querySelector(".flag_details").append(country)
-   
-    const population=document.createElement("p")
-    population.innerText="population:"+countries[i].population
-    document.body.append(population)
-   
-    const region=document.createElement("p")
-    region.innerText="Region:"+countries[i].region
-    document.body.append(region)
-   
-    const capital=document.createElement("p")
-    capital.innerText="Capital:"+countries[i].capital
-    document.body.append(capital)
-   
-    document.body.querySelector(".flag_details").append(country,population,region,capital)
+ 
+document.body.innerHTML=`
+<section class="container">
+<div id="flags" class="row">
+</div>
+</section>`
+countries.forEach(data => {
+const flags=document.querySelector("#flags")
+  flags.innerHTML+=`
+  <div class="col-12 col-sm-6 col-md-3 col-lg-3">
+  <div class="flag-container>
+  <img 
+    src=${data.image} 
+    class="flag"
+  />
+  <h3>${data.country}</h3>
+  <p><b>population:</b>${data.population}</p>
+  <p><b>region:</b>${data.region}</p>
+  <p><b>capital:</b>${data.capital}</p>
+  </div></div>`
 });
